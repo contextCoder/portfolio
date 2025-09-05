aa4cad71ef96a9f12b5a1fa19dfc25290a3633f4
Tukaram Todkari <tukatodkari@gmail.com>
2025-09-05 15:44:14 +0530

    Day-3 : Fix basename localtion to app.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Hero from "./components/homePage/Hero";
 export default function App() {
   console.log("Environment:", process.env.REACT_APP_NODE_ENV);
   return (
-    <Router>
-      <Routes basename={process.env.REACT_APP_NODE_ENV === "production" ? "/portfolio" : "/"}>
+    <Router basename={process.env.REACT_APP_NODE_ENV === "production" ? "/portfolio" : "/"}>
+      <Routes>
         <Route path="/" element={<Hero />} />
       </Routes>
     </Router>
